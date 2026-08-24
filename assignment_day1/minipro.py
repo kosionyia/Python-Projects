5576fec3d399a83609540fcac871e31e625dc10b
Kosisochukwu <onyiakosisochi@gmail.com>
2026-08-24 09:20:10 +0100

    Delete assignment_day1 directory
    Mistakenly pushed day 1 into day 2 so i had to delete.

--- assignment_day1/minipro.py
+++
@@ -1,6 +0,0 @@
-word = input("Enter a word or phrase")
-new_word = word.strip().lower()
-if new_word == new_word[::-1]:
-    print(f"{new_word} is a palindrome")
-else:
-    print(f"{new_word} is not a palindrome")
