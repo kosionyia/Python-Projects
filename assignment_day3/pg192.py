48464eb590d6dcca2fa4bdc0809475c6b436988e
kosionyia <onyiakosisochi@gmail.com>
2026-08-24 14:29:26 +0100

    first commit for page 192

---
+++ assignment_day3/pg192.py
@@ -0,0 +1,13 @@
+# Create a dictionary with information about a person
+person = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 25,
+    "city": "Lagos"
+}
+
+# Print each piece of information
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
