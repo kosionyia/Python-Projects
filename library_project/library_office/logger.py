8b25c0877c103dce4c172590568135476e17d4bb
Kosisochukwu <onyiakosisochi@gmail.com>
2026-08-31 10:06:16 +0100

    Delete library_project directory

--- library_project/library_office/logger.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-LOG_FILE = "window_ledger.txt"
-
-def log (username, request, code):
-    time = datetime.now().strftime()
-
-    with open(LOG_FILE, "a") as file:
-        file.write(f"{time} | {username} | {request} | {code}")
